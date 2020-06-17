c33ed499c3d2c084665b12a2405fa2109ab24d6c
P-Brzezinski <pawelbrzezinski1987@gmail.com>
2020-06-17 22:13:57 +0200

    Add database and repositories

---
+++ src/main/java/pl/brzezinski/noteit/db/NotebookRepository.java
@@ -0,0 +1,11 @@
+package pl.brzezinski.noteit.db;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pl.brzezinski.noteit.model.Notebook;
+
+import java.util.UUID;
+
+@Repository
+public interface NotebookRepository extends JpaRepository<Notebook, UUID> {
+}
