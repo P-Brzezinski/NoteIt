a591129dcf58e5c83eef040ead8efc0734d43b0a
P-Brzezinski <pawelbrzezinski1987@gmail.com>
2020-06-17 21:52:01 +0200

    Add model classes

---
+++ src/test/java/pl/brzezinski/noteit/NoteitApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.brzezinski.noteit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
