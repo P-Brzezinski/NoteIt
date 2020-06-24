67a1c656b922419b58e600961f761a66d87f3902
P-Brzezinski <pawelbrzezinski1987@gmail.com>
2020-06-24 22:44:30 +0200

    Add notebooks display/edit/delete options

--- noteIt-ng-app/src/app/notes/notes.component.ts
+++ noteIt-ng-app/src/app/notes/notes.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Notebook} from './model/notebook';
 import {ApiService} from '../shared/api.service';
@@ -11,13 +11,14 @@ import {ApiService} from '../shared/api.service';
 export class NotesComponent implements OnInit {
   notebooks: Notebook[] = [];
 
-  constructor(private apiService: ApiService) { }
+  constructor(private apiService: ApiService) {
+  }
 
   ngOnInit(): void {
     this.getAllNotebooks();
   }
 
-  public getAllNotebooks(){
+  public getAllNotebooks() {
     this.apiService.getAllNotebooks().subscribe(
       res => {
         this.notebooks = res;
@@ -26,5 +27,47 @@ export class NotesComponent implements OnInit {
         alert('An error has occurred');
       }
     );
+  }
+
+  createNotebook() {
+    const newNotebook: Notebook = {
+      name: 'New notebook',
+      id: null,
+      numberOfNotes: 0
+    };
+
+    this.apiService.postNoteBook(newNotebook).subscribe(
+      res => {
+        newNotebook.id = res.id;
+        this.notebooks.push(newNotebook);
+      },
+      error => {
+        alert('An error has occurred while saving notebook');
+      }
+    );
+  }
+
+  updateNotebook(updatedNotebook: Notebook) {
+    this.apiService.postNoteBook(updatedNotebook).subscribe(
+      res => {
+      },
+      error => {
+        alert('An error has occurred while saving notebook');
+      }
+    );
+  }
+
+  deleteNotebook(notebook: Notebook) {
+    if (confirm('Are you sure you want to delete notebook?')) {
+      this.apiService.deleteNotebook(notebook.id).subscribe(
+        res => {
+          const indexOfNotebook = this.notebooks.indexOf(notebook);
+          this.notebooks.splice(indexOfNotebook, 1);
+        },
+        err => {
+          alert('Could not delete network');
+        }
+      );
+    }
   }
 }
