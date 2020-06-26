5aa6abe6af630ed3278f9c67d749de60e97eb36e
P-Brzezinski <pawelbrzezinski1987@gmail.com>
2020-06-26 22:11:50 +0200

    War package

--- noteIt-api/src/main/java/pl/brzezinski/noteit/NoteitApplication.java
+++ noteIt-api/src/main/java/pl/brzezinski/noteit/NoteitApplication.java
@@ -2,11 +2,13 @@ package pl.brzezinski.noteit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class NoteitApplication {
+public class NoteitApplication extends SpringBootServletInitializer {
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
         SpringApplication.run(NoteitApplication.class, args);
     }
 
